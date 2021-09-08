eebfdc4459a75e28b1efda5044ae47517fefb9d3
Titus <ltvminhtuan99@gmail.com>
2021-09-09 01:22:41 +0700

    additional commit

---
+++ model/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+//block password
+const bcrypt = require("bcrypt");
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 6,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+    },
+
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        required: true,
+    },
+});
+// block passs
+UserSchema.pre("save", function(next) {
+    if (!this.isModified("password")) return next();
+    bcrypt.hash(this.password, 10, (err, passwordHash) => {
+        if (err) return next(err);
+        this.password = passwordHash;
+        next();
+    });
+});
+UserSchema.methods.comparePassword = function(password, cb) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if (err) return cb(err);
+        else {
+            if (!isMatch) return cb(null, isMatch);
+            return cb(null, this);
+        }
+    });
+};
+module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -7,6 +7,17 @@ mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected..."))
     .catch((err) => console.log(err));
-
+//TEST model
+const User = require("./model/User");
+const userPut = {
+    username: "nguyenminhtuan",
+    password: "123456",
+    role: "admin",
+};
+const user = new User(userPut);
+user.save((err, data) => {
+    if (err) console.log(err);
+    console.log(data);
+});
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`SERVER RUN WITH PORT ${PORT}`));
